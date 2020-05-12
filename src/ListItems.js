097fc76ea8016c09e387e23d28dd70cd02933abd
VivekDoshi <vdoshi1994@gmail.com>
2020-05-12 14:19:26 +0530

    first commit

---
+++ src/ListItems.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './ListItems.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+function ListItems(props){
+    const items = props.items;
+    const listItems = items.map(item => 
+    {
+        return <div className='list' key={item.key}>
+            <p>
+                <input 
+                    type='text' 
+                    id={item.key} 
+                    value = {item.text}
+                    onChange = {
+                        (event) => {
+                            props.setUpdate(event.target.value, item.key)
+                        }
+                    } 
+                />
+                <span>
+                    <FontAwesomeIcon className='faicons' 
+                     icon='trash'
+                     onClick={ () => props.deleteItem(item.key)}
+                    />
+                </span>
+            </p>
+        </div>
+    })
+    return(   
+        <div>{listItems}</div>
+    )
+}
+
+export default ListItems;
